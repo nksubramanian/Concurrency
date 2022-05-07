2610439ce0a63f35708ee4a09fef2f0ee7cd909f
subramanian.kumarappan <subramanian.nk@live.com>
2022-05-07 14:26:27 +0530

    first commit

---
+++ ConcurrencyExercise/ConcurrencyExercise/ConOne.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrencyExercise
+{
+    internal class ConOne
+    {
+        public void ConOneMain()
+        {
+            Console.WriteLine("Inside Con one Main");
+            Calculate1();
+            Calculate2();
+            Calculate3();
+        }
+
+        public void ConOneMain1()
+        {
+            Console.WriteLine("Inside 1 Main");
+
+
+            Task.Run(() => { Calculate1(); });
+            Task.Run(() => { Calculate2(); });
+            Task.Run(() => { Calculate3(); });
+
+
+
+        }
+
+
+
+        public void Calculate1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Inside Calculate1");
+        }
+
+        public void Calculate2()
+        {
+            Console.WriteLine("Inside Calculate2");
+        }
+
+        public void Calculate3()
+        {
+            Console.WriteLine("Inside Calculate3");
+        }
+
+    }
+}
---
+++ ConcurrencyExercise/ConcurrencyExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConcurrencyExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConOne().ConOneMain();
+            new ConOne().ConOneMain1();
+            Console.Read();
+        }
+    }
+}
